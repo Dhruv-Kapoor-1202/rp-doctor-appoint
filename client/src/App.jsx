05c52620cc72c2d503418e6cf619364038f255de
Dhruv-Kapoor-1202 <dhruv1690.be21@chitkara.edu.in>
2024-11-19 04:53:41 +0530

    chore:

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,51 @@
 import { Route, Routes } from "react-router-dom";
 import LandingPage from "@/pages/LandingPage";
-import DashBoardLayout from "@/pages/DashBoardLayout";
-import Patient from "./pages/Patient";
-import Staff from "./pages/Staff";
-import Reports from "./pages/Reports";
-import Team from "./pages/Team";
+// import DashBoardLayout from "@/pages/DashBoardLayout";
+// import Patient from "./pages/Patient";
+// import Staff from "./pages/Staff";
+// import Reports from "./pages/Reports";
+// import Team from "./pages/Team";
 import SignupPage from "./pages/SignupPage";
 import LoginPage from "./pages/LoginPage";
 import { useSelector } from "react-redux";
+import PublicRoute from "./components/PublicRoute";
+// import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   const { loading } = useSelector((state) => state.alerts);
   return (
     <>
-      {loading ? <p>loading...</p> : <p>Stuff Happens</p>}
-      <Routes>
+      {loading ? (
+        <p>loading...</p>
+      ) : (
+        <Routes>
+          <Route
+            path="/login"
+            element={
+              <PublicRoute>
+                <LoginPage />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="/register"
+            element={
+              <PublicRoute>
+                <SignupPage />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="/"
+            element={
+              <PublicRoute>
+                <LandingPage />
+              </PublicRoute>
+            }
+          />
+        </Routes>
+      )}
+      {/* <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/dashboard" element={<DashBoardLayout />}>
           <Route path="/dashboard/patient" element={<Patient />} />
@@ -24,7 +55,7 @@ function App() {
         </Route>
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/login" element={<LoginPage />} />
-      </Routes>
+      </Routes> */}
     </>
   );
 }
